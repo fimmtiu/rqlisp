6133906d958269d686f57bf6ee1a3c9e732c256f
Dennis Taylor <dennis.taylor@clio.com>
2019-11-01 11:04:04 -0500

    Very basic, not-very-good arity checking.

--- lib/rqlisp/function.rb
+++ lib/rqlisp/function.rb
@@ -4,7 +4,12 @@ module Rqlisp
 
     def initialize(env:, args:, code:)
       @env = env
+
       @args = args
+      # FIXME: This arg counting is very inexact, and should be tightened up later.
+      @rest_var = args.to_array.last if args.to_array.include?(var("&rest"))
+      @arity = args.to_array.slice_before("&rest").first&.count || 0
+
       if code.is_a?(Method)
         @code = code
       else
@@ -43,17 +48,16 @@ module Rqlisp
     def env_with_funcall_args(arg_values, env)
       new_local_env = Rqlisp::Env.new(parent: env)
 
-      if arg_values.length < @args.length
+      if arg_values.length < @arity
         raise "Not enough arguments to #{self}! Expected #{@args.length}, got #{arg_values.length}"
-      elsif !has_rest_args? && arg_values.length > @args.length
+      elsif !has_rest_args? && arg_values.length > @arity
         raise "Too many arguments to #{self}! Expected #{@args.length}, got #{arg_values.length}"
       end
 
       @args.to_array.each_with_index do |arg_name, i|
         if arg_name == var("&rest")
-          rest_var = @args.to_array.last
           rest_values = list(*arg_values[i..-1])
-          new_local_env.define(rest_var, rest_values)
+          new_local_env.define(@rest_var, rest_values)
           break
         else
           new_local_env.define(arg_name, arg_values[i])
